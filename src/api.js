3324890837458fbcb6c652f7e42801c0bb5c1256
Ahir Bansariben Vrajlal <141428999+Bansariahir123@users.noreply.github.com>
2025-11-23 08:54:37 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,5 +1,7 @@
 import axios from "axios";
-export default axios.create({ baseURL: "https://api.jsonbin.io/v3/b/692277c6d0ea881f40f9fbde/latest" },headers: {
-    "X-Master-Key": "$2a$10$cHUWN8iJGK.7M9ljLVOgl.3iGOQ4euuuxkqF9sBIkhBATsr3Bifv6",
-    "Content-Type": "application/json"
-  });
+export default axios.create({ 
+  baseURL: "https://api.jsonbin.io/v3/b/692277c6d0ea881f40f9fbde/latest" 
+}, headers: {
+  "X-Master-Key": "$2a$10$cHUWN8iJGK.7M9ljLVOgl.3iGOQ4euuuxkqF9sBIkhBATsr3Bifv6",
+});
+
