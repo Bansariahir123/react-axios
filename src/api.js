be4d8464243f3b3841f8a3a7007f74ebd461d976
Ahir Bansariben Vrajlal <141428999+Bansariahir123@users.noreply.github.com>
2025-11-23 08:28:41 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,2 +1,2 @@
 import axios from "axios";
-export default axios.create({ baseURL: "http://localhost:5000" });
+export default axios.create({ baseURL: "https://api.jsonbin.io/v3/b/692277c6d0ea881f40f9fbde/latest" });
